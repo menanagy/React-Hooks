d3f3c63c9d9d06d798647409fb1db033fe6210ae
Mina Nagy <minanagy1993@gmail.com>
2021-04-08 01:07:52 +0200

    React Hooks Session1 (useState)

--- src/App.js
+++ src/App.js
@@ -1,27 +1,37 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import Header from './Header';
-export default class App extends Component{
-  constructor(props){
-    super(props);
-    this.state={
-      counter:0
+
+  //Before Hooks Can't use state in Function Component 
+  // Function Component  No this , No render()function
+  //for Make State using useState(useState==>is Func hook into React Feature)
+  /* 
+      Not Exact Just Imagne
+      function useState(initalValue){
+        let item =initalValue;
+        let setItem(val)=>{
+          item=val;
+        }
+        return [item,setItem] ; 
+        //Trigger React Rendring 
+
+      }
+  
+  */
+function App(){
+  const [counter,setCounter]=useState(0);//0==>intial Value
+    function countUp(){
+      setCounter(counter+1);
     };
-    /*Solve */
-   //Method 1 
-    this.countUp=this.countUp.bind(this);//bind==>Change Context (Countup Calling in Another Context)[without bind this==>undefined]
-   //Method 2
-   //using countup=()=>{} arrow Function
-  }
-  countUp(){
-    console.log(this);
-    this.setState({counter:++this.state.counter});//
-  };
-  render(){
-    return(
-      <div className="container text-center pt-5">
-        <button className="btn btn-primary" onClick={this.countUp}>Count UP + </button>
-        <h1 className="mt-3">{this.state.counter}</h1>
-        <Header header='Hi Mina'/>
-      </div>   
-    )};
+
+      return(
+        <div className="container text-center pt-5">
+          <button className="btn btn-primary" onClick={countUp}>Count UP + </button>
+          <h1 className="mt-3">{counter}</h1>
+          <Header header='Hi Mina'/>
+        </div>   
+      );
+
 }
+
+
+export default App;
