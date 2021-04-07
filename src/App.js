fec10850e3ee104d3aa0427c5735002fe67dea93
Mina Nagy <minanagy1993@gmail.com>
2021-04-08 00:13:22 +0200

    Class Component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import React from 'react';
-function App() {
-  return (
-   <div className="text-center">
-    <h1 >Hello Mina</h1>
-    <button className="btn btn btn-success">Heloo</button>
-   </div>
-  );
+import React, { Component } from 'react';
+export default class App extends Component{
+  constructor(props){
+    super(props);
+    this.state={
+      counter:0
+    };
+    
+  }
+  countUp=()=>{
+    this.setState({counter:++this.state.counter});
+  };
+  render(){
+    return(
+      <div className="container text-center pt-5">
+        <button className="btn btn-primary" onClick={this.countUp}>Count UP + </button>
+        <h1 className="mt-3">{this.state.counter}</h1>
+      </div>   
+    )};
 }
-
-export default App;
